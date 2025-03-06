971ec7ef13d5e7aa36739d5440c201b638543c8a
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-03-06 17:07:49 +0900

    전체상품#1 개수 카운트

---
+++ kickdeal/src/pages/product.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import '../pagescss/all.css';
+import React, { useState ,useEffect } from 'react'; 
+
+
+function Product() {
+
+  const [row,setrow] = useState();
+
+  useEffect(() => {
+    axios.get('https://port-0-kickdeal2-m1qhzohka7273c65.sel4.cloudtype.app/product')
+    .then((response)=>{
+      setrow(response.data[response.data.length - 1].id)
+    })
+    
+  })
+
+  return (
+      <div className="main">
+        <p>
+          <div>
+            
+            <button>보기</button>
+          
+          </div>
+        <p>{row}</p>
+            
+        </p>
+      </div>
+
+  );
+}
+
+export default Product;
